c2e86afaa3d784dc3d221a6e3bb4b0539c492f1f
Evan Pendergraft <evanpendergraft@gmail.com>
2025-07-22 21:59:25 -0400

    added some example problems

---
+++ ex1.py
@@ -0,0 +1,5 @@
+# the code below almost works
+prinq("hello world")
+
+# Desired Output
+# "hello world"
---
+++ ex2-3.py
@@ -0,0 +1,5 @@
+# Write a program which prompts the user for a Celsius temperature, convert the temperature to Fahrenheit, and print out the converted temperature.
+
+
+# example output
+# 0 Celsius is equal to 32 Fahrenheit
